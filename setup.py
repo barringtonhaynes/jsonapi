73f29901bb3e6a0a3b560e7883da524bfeb51b24
Barrington Haynes <barrington@littledog.co>
2023-08-24 21:38:08 +0100

    Update pydantic version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsonapi',
-    version='0.2',
+    version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A Python package for working with JSON API and FastAPI',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
-        'pydantic==2.2.1',
+        'pydantic==2.2.0',
         'fastapi==0.101.1'
     ],
     url='https://github.com/barringtonhaynes/jsonapi',
