4a59091c27352bcf3769da3f166a748b258feb7b
Barrington Haynes <barrington@littledog.co>
2023-08-24 18:44:24 +0100

    Update versions of python due to github action conflict

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsonapi',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A Python package for working with JSON API and FastAPI',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
-        'pydantic==2.3.0',
+        'pydantic==2.2.1',
         'fastapi==0.101.1'
     ],
     url='https://github.com/barringtonhaynes/jsonapi',
