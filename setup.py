607864965c8b521d1274322e203b38a45c05166c
Barrington Haynes <barrington@littledog.co>
2023-08-25 17:00:32 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsonapi',
-    version='0.4',
+    version='0.5',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A Python package for working with JSON API and FastAPI',
